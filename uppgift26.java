672beadc8d65757b74c498b3007b258bb63f1634
Elon Nygren <elon.nygren@learnet.se>
2019-09-19 11:19:56 +0200

    uppgift2_6

---
+++ uppgift26.java
@@ -0,0 +1,15 @@
+
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class uppgift26 { 
+	public static void main(String[] args) {
+		char c1='\u00d8';
+		char c2='\u00f8';
+		JOptionPane.showMessageDialog(null, ""+c1);
+		JOptionPane.showMessageDialog(null, ""+c2);
+		
+		
+	}
+}
---
+++ uppgift27.java
@@ -0,0 +1,16 @@
+
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class uppgift27 { 
+	public static void main(String[] args) {
+		char c1='\u00d8';
+		char c2='\u00f8';
+		char c3='\u03c0';
+		JOptionPane.showMessageDialog(null, ""+c1);
+		JOptionPane.showMessageDialog(null, ""+c2);
+		JOptionPane.showMessageDialog(null, ""+c3);
+		
+	}
+}
